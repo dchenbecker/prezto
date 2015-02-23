50fabcd03be4512c7e72697b87e0e48752d73224
Derek Chen-Becker <derek@simpleenergy.com>
2015-02-22 22:46:48 -0700

    Adding my own customizations

---
+++ runcoms/custom/remux.zsh
@@ -0,0 +1,10 @@
+# Nice snippet for reattaching tmux to ssh-agent
+# http://justinchouinard.com/blog/2010/04/10/fix-stale-ssh-environment-variables-in-gnu-screen-and-tmux/
+function remux() {
+  if [[ -n $TMUX ]]; then
+    NEW_SSH_AUTH_SOCK=`=tmux showenv|grep '^SSH_AUTH_SOCK'|cut -d = -f 2`
+    if [[ -n $NEW_SSH_AUTH_SOCK ]] && [[ -S $NEW_SSH_AUTH_SOCK ]]; then
+      export SSH_AUTH_SOCK=$NEW_SSH_AUTH_SOCK
+    fi
+  fi
+}
