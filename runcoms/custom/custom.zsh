c243b1718d6cac114c2e916ec006278b6102d2de
Derek Chen-Becker <derek@simpleenergy.com>
2015-05-24 13:37:31 -0600

    Fix up a few custom setup items

--- runcoms/custom/custom.zsh
+++ runcoms/custom/custom.zsh
@@ -1,28 +1,28 @@
 # Special dircolors
 if [ -r ~/.dircolors ]; then
-  eval `dircolors ~/.dircolors`
+  eval "$(dircolors ~/.dircolors)"
 fi
 
 # Derek's Linux aliases
-if [ $(uname -s) != "Darwin" ]; then
+if [ "$(uname -s)" != "Darwin" ]; then
   alias ack='ack-grep'
 fi
 
 # Derek's OSX aliases
-if [ $(uname -s) = "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
   alias emacs='/Applications/Emacs.app/Contents/MacOS/Emacs'
   alias emacsclient='/Applications/Emacs.app/Contents/MacOS/bin/emacsclient'
   export EDITOR="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient"
-  export PATH=$PATH:/usr/local/texlive/2014basic/bin/x86_64-darwin
+  export PATH=$PATH:/usr/local/texlive/2014basic/bin/x86_64-darwin:/usr/local/go/bin
   alias tar='gtar'
 fi
 
 alias cstags='ctags -eR --languages="c#"'
 alias cssh='~/.oh-my-zsh/custom/tmux-cssh/tmux-cssh -ss synchome.sh'
-alias ctags='ctags --languages=scala,java,python,puppet -R'
+alias ctags='ctags --languages=scala,java,python,puppet -R --exclude=.ensime_cache'
 alias curlapi="curl -H 'Content-Type: application/json'"
 alias egrep='egrep --color=auto'
-alias etags='ctags -e --languages=scala,java,python,puppet -R'
+alias etags='ctags -e'
 alias fgrep='fgrep --color=auto'
 alias gfa='git fetch --all -p'
 alias go='git checkout'
@@ -33,6 +33,7 @@ alias la='ls -la'
 alias ll='ls -l'
 alias ls='ls --color=auto'
 alias mv='mv -i'
+alias qe="emacs -q -nw"
 alias revelation='keepassx'
 alias sbt='nocorrect sbt'
 alias scp='rsync -vazP'
@@ -40,7 +41,7 @@ alias screen='runtmux'
 alias syh='synchome.sh'
 alias tmux='runtmux'
 alias top='htop'
-alias vi="$EDITOR"
+alias vi="\$EDITOR"
 
 # I want globbing with rsync, prezto
 unalias rsync
@@ -86,7 +87,7 @@ WORDCHARS=""
 unset VISUAL
 
 # Fix up the sorin prompt the way I like it
-PROMPT='${SSH_TTY:+"%F{red}%n%f@%F{yellow}%m%f "}%F{cyan}${_prompt_sorin_pwd}%f${git_info:+${(e)git_info[prompt]}}%(!. %B%F{red}#%f%b.)${git_info[rprompt]}${editor_info[keymap]} '
+export PROMPT='${SSH_TTY:+"%F{red}%n%f@%F{yellow}%m%f "}%F{cyan}${_prompt_sorin_pwd}%f${git_info:+${(e)git_info[prompt]}}%(!. %B%F{red}#%f%b.)${git_info[rprompt]}${editor_info[keymap]} '
 #RPROMPT='${editor_info[overwrite]}%(?:: %F{red}⏎%f)${VIM:+" %B%F{green}V%f%b"}${INSIDE_EMACS:+" %B%F{green}E%f%b"}'
 unset RPROMPT
 
@@ -94,7 +95,14 @@ unset RPROMPT
 export LESS='-F -g -i -M -R -X -z-4'
 
 
-# boot2docker is *such* a pain: https://github.com/boot2docker/boot2docker/issues/628#issuecomment-68909772
-DOCKER_HOST=tcp://127.0.0.1:2376
-DOCKER_CERT_PATH=/Users/dchenbecker/.boot2docker/certs/boot2docker-vm
-DOCKER_TLS_VERIFY=1
+# boot2docker init
+if which boot2docker > /dev/null && [ $(boot2docker status) = "running" ]; then
+    $(boot2docker shellinit 2> /dev/null)
+fi
+
+# VirtualEnv Wrapper
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    export PROJECT_HOME=$HOME/SimpleEnergy
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
